cdf93b398f0566767f1ed843af85395b655d9032
Italiz <italizv@gmail.com>
2023-03-22 19:22:06 -0400

    Complete conference entity

--- events/api_views.py
+++ events/api_views.py
@@ -47,6 +47,7 @@ class ConferenceListEncoder(ModelEncoder):
     properties = ["name"]
 
 
+@require_http_methods(["GET", "POST"])
 def api_list_conferences(request):
     """
     Lists the conference names and the link to the conference.
@@ -66,13 +67,34 @@ def api_list_conferences(request):
         ]
     }
     """
-    conferences = Conference.objects.all()
-    return JsonResponse(
-        {"conferences": conferences},
-        encoder=ConferenceListEncoder,
-    )
+    if request.method == "GET":
+        conferences = Conference.objects.all()
+        return JsonResponse(
+            {"conferences": conferences},
+            encoder=ConferenceListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+
+        # Get the Location object and put it in the content dict
+        try:
+            location = Location.objects.get(id=content["location"])
+            content["location"] = location
+        except Location.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid location id"},
+                status=400,
+            )
+
+        conference = Conference.objects.create(**content)
+        return JsonResponse(
+            conference,
+            encoder=ConferenceDetailEncoder,
+            safe=False,
+        )
 
 
+@require_http_methods(["DELETE", "GET", "PUT"])
 def api_show_conference(request, id):
     """
     Returns the details for the Conference model specified
@@ -98,10 +120,24 @@ def api_show_conference(request, id):
         }
     }
     """
-    conference = Conference.objects.get(id=id)
-    return JsonResponse(
-        conference, encoder=ConferenceDetailEncoder, safe=False
-    )
+    if request.method == "GET":
+        conference = Conference.objects.get(id=id)
+        return JsonResponse(
+            conference, encoder=ConferenceDetailEncoder, safe=False
+        )
+    elif request.method == "DELETE":
+        count, _ = Conference.objects.filter(id=id).delete()
+        return JsonResponse({"deleted": count > 0})
+    # request is PUT
+    else:
+        # create a new request
+        content = json.loads(request.body)
+        # find the body object
+        Conference.objects.filter(id=id).update(**content)
+        conference = Conference.objects.get(id=id)
+        return JsonResponse(
+            {"conference": conference}, encoder=ConferenceDetailEncoder
+        )
 
 
 @require_http_methods(["GET", "POST"])
